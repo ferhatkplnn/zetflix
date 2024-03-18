2029cf349c24d73f65714fbab53cbb70768e3438
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-18 15:07:07 +0300

    feat(Input): Create Input component for form inputs
    - Created Input component to handle form input fields
    - Implemented dynamic label rendering for better accessibility

---
+++ client/src/components/shared/Input.tsx
@@ -0,0 +1,26 @@
+import { ComponentPropsWithoutRef } from "react";
+
+type InputProps = ComponentPropsWithoutRef<"input"> & {
+  label: string;
+};
+
+function Input({ label, type }: InputProps) {
+  return (
+    <div className="relative">
+      <input
+        id={label}
+        type={type}
+        placeholder=""
+        className="peer bg-black bg-opacity-50 px-4 pt-6 pb-2 w-full rounded border border-slate-600 text-white focus:ring-2 focus:ring-white outline-none invalid:border-red-500"
+      />
+      <label
+        htmlFor={label}
+        className="absolute text-slate-400 left-3 scale-75 top-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:top-4 peer-focus:scale-75 peer-focus:top-2 duration-300"
+      >
+        {label}
+      </label>
+    </div>
+  );
+}
+
+export default Input;
