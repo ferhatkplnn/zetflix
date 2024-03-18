23dec8cf381b4a66a19e3002dec90b10a8d6e366
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-18 13:15:53 +0300

    feat: Create Home page component
    - Import HomeBanner and NavBar components
    - Implement Home function component to render NavBar and HomeBanner

---
+++ client/src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import HomeBanner from "./components/HomeBanner";
+import NavBar from "../../components/navbar/Navbar";
+
+function Home() {
+  return (
+    <div>
+      <NavBar />
+      <HomeBanner />
+    </div>
+  );
+}
+
+export default Home;
