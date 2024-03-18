7fb128c5e5742421aedeefba00cea71b2797de26
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-18 17:12:42 +0300

    feat: Create Plans page to display subscription plans
    - Implemented Plans component to display available subscription plans
    - Utilized PlanCard component for each plan representation

---
+++ client/src/pages/plans/Plans.tsx
@@ -0,0 +1,34 @@
+import PlanCard from "./components/PlanCard";
+
+const PLANS = [
+  { type: "Basic", price: "$5.99", downloads: true, southPark: false },
+  { type: "Premium", price: "$10.99", downloads: true, southPark: true },
+];
+
+function Plans() {
+  return (
+    <div className="flex items-center h-screen justify-center">
+      <div className="w-[600px]">
+        <h1 className="font-semibold text-3xl">
+          Choose a plan that works for you
+        </h1>
+        <div className="flex mt-4">
+          {PLANS.map((plan, index) => (
+            <PlanCard
+              key={index}
+              type={plan.type}
+              price={plan.price}
+              downloads={plan.downloads}
+              southPark={plan.southPark}
+            />
+          ))}
+        </div>
+        <button className="bg-red-500 py-3 text-white rounded-md w-full mt-4 hover:bg-red-700 cursor-pointer duration-300">
+          Purchase
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Plans;
