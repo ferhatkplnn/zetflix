6a30c60e483ca73ebe85092dac597229b23b4c93
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-20 01:35:01 +0300

    feat: Implement lazy loading in MovieList component
    - Added lastElementRef prop to MovieList component to determine the last movie element for lazy loading
    - Updated MovieListCard components to pass lastElementRef prop to the last movie element for triggering lazy loading

--- client/src/pages/browse/components/MovieList.tsx
+++ client/src/pages/browse/components/MovieList.tsx
@@ -1,19 +1,30 @@
 import { Movie } from "../../../types";
 import MovieListCard from "./MovieListCard";
 
-function MovieList({ movies }: { movies: Movie[] }) {
+function MovieList({
+  movies,
+  lastElementRef,
+}: {
+  movies: Movie[];
+  lastElementRef: (node: HTMLDivElement) => void;
+}) {
   return (
     <div className="px-12 mt-4 space-y-8">
       <div>
         <p className="text-black text-2xl font-semibold mb-4">Popular Shows</p>
         <div className="grid sm:grid-cols-2 xl:grid-cols-4 md:grid-cols-3  gap-2">
           {movies &&
-            movies.map((movie) => (
-              <MovieListCard key={movie.id} movie={movie} />
+            movies.map((movie, index) => (
+              <MovieListCard
+                key={movie.id}
+                movie={movie}
+                lastElementRef={
+                  movies.length === index + 1 ? lastElementRef : null
+                }
+              />
             ))}
         </div>
       </div>
-      <div className="h-56"></div>
     </div>
   );
 }
