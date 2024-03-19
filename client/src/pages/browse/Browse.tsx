534b18b34ae22a7e76273233527e0bb593e717cb
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-20 01:34:25 +0300

    feat: Implement lazy loading of movies in Browse component
    - Added lazy loading feature to dynamically fetch more movies as user scrolls down
    - Implemented IntersectionObserver to detect when user reaches the bottom of the page
    - Updated Browse component to use offset parameter for fetching movies progressively

--- client/src/pages/browse/Browse.tsx
+++ client/src/pages/browse/Browse.tsx
@@ -1,19 +1,35 @@
+import { useCallback, useRef, useState } from "react";
 import NavBar from "../../components/navbar/Navbar";
 import useMoviesList from "../../hooks/useMoviesList";
 import BillBoard from "./components/BillBoard";
 import MovieList from "./components/MovieList";
 
 function Browse() {
-  const { data, loading, error } = useMoviesList();
+  const [offset, setOffset] = useState<number>(12);
+  const { data, loading, error } = useMoviesList(offset);
+
+  const observer = useRef<null | IntersectionObserver>(null);
+
+  const lastElementRef = useCallback((node: HTMLDivElement) => {
+    if (observer.current) observer.current.disconnect();
+
+    observer.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        setOffset((prevOffset) => prevOffset + 12);
+      }
+    });
+
+    if (node) observer.current.observe(node);
+  }, []);
 
   return (
     <div>
       <NavBar />
       <BillBoard />
       <div className="pb-5">
-        {loading && <p>Loading...</p>}
         {error && <p>{error}</p>}
-        {data && <MovieList movies={data} />}
+        {data && <MovieList movies={data} lastElementRef={lastElementRef} />}
+        {loading && <p>Loading...</p>}
       </div>
     </div>
   );
