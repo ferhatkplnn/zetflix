1e67e4371ab204c694a7c4bd2bb2e83393b92efc
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-19 13:53:54 +0300

    feat: Integrate dynamic movie details and navigation in Watch page
    - Implemented useParams and useNavigate hooks for accessing URL parameters and navigation
    - Used useMovie hook to fetch movie details dynamically based on URL parameter
    - Added error and loading state handling for user feedback
    - Implemented functionality to navigate back to the previous page on clicking the back button

--- client/src/pages/watch/Watch.tsx
+++ client/src/pages/watch/Watch.tsx
@@ -1,17 +1,33 @@
 import { FaArrowLeft } from "react-icons/fa";
+import useMovie from "../../hooks/useMovie";
+import { useNavigate, useParams } from "react-router-dom";
 
 function Watch() {
+  const { id } = useParams() as { id: string };
+  const { data, error, loading } = useMovie(id);
+
+  const navigate = useNavigate();
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error || !data) return <p>{error}</p>;
+
+  const { title, videoUrl } = data;
+
   return (
     <div className="h-screen w-screen bg-black">
       <nav className="fixed w-full p-4 z-10 flex items-center gap-8 bg-black bg-opacity-80">
-        <FaArrowLeft className="w-10 text-white cursor-pointer hover:opacity-80 transition" />
+        <FaArrowLeft
+          onClick={() => navigate(-1)}
+          className="w-10 text-white cursor-pointer hover:opacity-80 transition"
+        />
         <p className="text-white text-3xl font-bold">
-          <span className="font-light">Watching:</span> South Park
+          <span className="font-light">Watching:</span> {title}
         </p>
       </nav>
       <iframe
         className="h-full w-full"
-        src="https://www.youtube.com/embed/MW-bASP8mAg?autoplay=1"
+        src={`https://www.youtube.com/embed/${videoUrl}?autoplay=1`}
         allowFullScreen
         allow="autoplay"
       ></iframe>
