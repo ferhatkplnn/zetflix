e2d3f602a1a19566bc14ce35cb8dd6b0654f6819
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-19 12:16:11 +0300

    feat: Refactor MovieListCard to accept movie object as prop
    - Updated MovieListCard component to accept a movie object containing title, description, duration, genre, thumbnailUrl, and videoUrl
    - Updated component structure to dynamically render movie details

--- client/src/pages/browse/components/MovieListCard.tsx
+++ client/src/pages/browse/components/MovieListCard.tsx
@@ -1,23 +1,23 @@
 import { FaPlay } from "react-icons/fa";
 import { SlArrowDown } from "react-icons/sl";
+import { Movie } from "../../../types";
 
-type MovieListCardProps = {
-  imgURL: string;
-};
+function MovieListCard({ movie }: { movie: Movie }) {
+  const { id, title, description, duration, genre, thumbnailUrl, videoUrl } =
+    movie;
 
-function MovieListCard({ imgURL }: MovieListCardProps) {
   return (
     <div className=" h-52 group">
       <img
-        className="object-cover object-center h-full w-full"
-        src={imgURL}
+        className="object-cover object-top h-full w-full"
+        src={thumbnailUrl}
         alt=""
       />
       <div className="relative ">
         <div className="absolute -top-60 scale-50 opacity-0 group-hover:-top-60 group-hover:opacity-100 invisible group-hover:visible  rounded-md group-hover:scale-105 overflow-hidden  w-full bg-neutral-900  text-white duration-150">
           <img
-            className="h-52 object-cover object-center w-full hw-full"
-            src={imgURL}
+            className="h-52 object-cover object-top w-full hw-full"
+            src={thumbnailUrl}
             alt=""
           />
           <div className="p-4">
@@ -30,14 +30,11 @@ function MovieListCard({ imgURL }: MovieListCardProps) {
               </button>
             </div>
             <div className="space-y-2">
-              <h2 className="font-bold text-3xl">Breaking Bad</h2>
-              <p className="text-xl text-gray-400">
-                A high School teacher decides to cook meth to provide for his
-                family before he dies of cancel.
-              </p>
+              <h2 className="font-bold text-3xl">{title}</h2>
+              <p className="text-xl text-gray-400">{description}</p>
               <div className="font-bold flex flex-col space-y-4">
-                <span>2 hours </span>
-                <span>Drama</span>
+                <span>{duration}</span>
+                <span>{genre}</span>
               </div>
             </div>
           </div>
