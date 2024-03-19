66e975fb8775750755f9c39cd4365059f8811369
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-19 11:36:23 +0300

    feat: Add CORS support to Express app
    - Integrate CORS middleware to handle cross-origin requests
    - Allows the application to respond to requests from different origins

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const movies = require("./movies.json");
 
 const app = express();
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send({ message: "Hello World!" });
