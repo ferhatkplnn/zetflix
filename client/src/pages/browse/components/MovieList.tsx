5bb0adcce95d3455acc8c4fbf3f0b25fb32eea6d
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-19 12:15:19 +0300

    feat: Make MovieList component dynamic by accepting movies as props
    - Refactored MovieList component to accept movies as props
    - Render MovieListCard component for each movie in the list

--- client/src/pages/browse/components/MovieList.tsx
+++ client/src/pages/browse/components/MovieList.tsx
@@ -1,34 +1,16 @@
+import { Movie } from "../../../types";
 import MovieListCard from "./MovieListCard";
 
-function MovieList() {
+function MovieList({ movies }: { movies: Movie[] }) {
   return (
     <div className="px-12 mt-4 space-y-8">
       <div>
         <p className="text-black text-2xl font-semibold mb-4">Popular Shows</p>
         <div className="grid sm:grid-cols-2 xl:grid-cols-4 md:grid-cols-3  gap-2">
-          <MovieListCard imgURL="https://m.media-amazon.com/images/M/MV5BMTJiMzgwZTktYzZhZC00YzhhLWEzZDUtMGM2NTE4MzQ4NGFmXkEyXkFqcGdeQWpybA@@._V1_QL75_UX500_CR0,0,500,281_.jpg" />
-
-          <MovieListCard imgURL="https://www.indyturk.com/sites/default/files/article/main_image/2023/11/23/1218301-1926200402.jpg" />
-
-          <MovieListCard imgURL="https://m.media-amazon.com/images/M/MV5BMTJiMzgwZTktYzZhZC00YzhhLWEzZDUtMGM2NTE4MzQ4NGFmXkEyXkFqcGdeQWpybA@@._V1_QL75_UX500_CR0,0,500,281_.jpg" />
-
-          <MovieListCard imgURL="https://www.indyturk.com/sites/default/files/article/main_image/2023/11/23/1218301-1926200402.jpg" />
-
-          <MovieListCard imgURL="https://m.media-amazon.com/images/M/MV5BMTJiMzgwZTktYzZhZC00YzhhLWEzZDUtMGM2NTE4MzQ4NGFmXkEyXkFqcGdeQWpybA@@._V1_QL75_UX500_CR0,0,500,281_.jpg" />
-
-          <MovieListCard imgURL="https://www.indyturk.com/sites/default/files/article/main_image/2023/11/23/1218301-1926200402.jpg" />
-
-          <MovieListCard imgURL="https://m.media-amazon.com/images/M/MV5BMTJiMzgwZTktYzZhZC00YzhhLWEzZDUtMGM2NTE4MzQ4NGFmXkEyXkFqcGdeQWpybA@@._V1_QL75_UX500_CR0,0,500,281_.jpg" />
-
-          <MovieListCard imgURL="https://www.indyturk.com/sites/default/files/article/main_image/2023/11/23/1218301-1926200402.jpg" />
-
-          <MovieListCard imgURL="https://m.media-amazon.com/images/M/MV5BMTJiMzgwZTktYzZhZC00YzhhLWEzZDUtMGM2NTE4MzQ4NGFmXkEyXkFqcGdeQWpybA@@._V1_QL75_UX500_CR0,0,500,281_.jpg" />
-
-          <MovieListCard imgURL="https://www.indyturk.com/sites/default/files/article/main_image/2023/11/23/1218301-1926200402.jpg" />
-
-          <MovieListCard imgURL="https://m.media-amazon.com/images/M/MV5BMTJiMzgwZTktYzZhZC00YzhhLWEzZDUtMGM2NTE4MzQ4NGFmXkEyXkFqcGdeQWpybA@@._V1_QL75_UX500_CR0,0,500,281_.jpg" />
-
-          <MovieListCard imgURL="https://www.indyturk.com/sites/default/files/article/main_image/2023/11/23/1218301-1926200402.jpg" />
+          {movies &&
+            movies.map((movie) => (
+              <MovieListCard key={movie.id} movie={movie} />
+            ))}
         </div>
       </div>
       <div className="h-56"></div>
