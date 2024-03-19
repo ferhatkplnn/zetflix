6cff17f1a953747ea21abb1c81ae9c4a3d75437b
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-19 12:33:40 +0300

    feat: Add dynamic background effect to NavBar
    - Implemented dynamic background effect in NavBar component based on scroll position
    - Navbar background changes to black with 80% opacity when scrolled past 900 pixels

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Logo from "./Logo";
 import NavItem from "./NavItem";
 
@@ -11,9 +12,25 @@ const TABS = [
 ];
 
 function NavBar() {
+  const [showBackground, setShowBackground] = useState<boolean>(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 900) {
+        setShowBackground(true);
+      } else {
+        setShowBackground(false);
+      }
+    });
+  }, []);
+
   return (
     <nav className="w-full fixed z-40">
-      <div className="px-16 py-6 flex items-center bg-black/80">
+      <div
+        className={`px-16 py-6 flex items-center ${
+          showBackground ? "bg-black/80" : ""
+        } duration-300`}
+      >
         <Logo />
         <div className="flex gap-7 ml-8">
           {TABS.map((tab, index) => (
