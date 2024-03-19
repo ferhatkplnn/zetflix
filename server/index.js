eb0c5d4e816371f03936d3a252b6f3400970a8e5
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-19 13:51:01 +0300

    feat: Add JSON parsing middleware, new movie endpoint, and 404 handler
    - Added express.json() middleware for JSON parsing
    - Created /movies/:id endpoint to retrieve a movie by ID
    - Implemented a 404 handler middleware for better error handling
    - Updated port assignment logic for flexibility and portability

--- server/index.js
+++ server/index.js
@@ -5,15 +5,30 @@ const movies = require("./movies.json");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send({ message: "Hello World!" });
+  res.json({ message: "Hello World!" });
 });
 
 app.get("/movies/list", (req, res) => {
-  return res.send(movies);
+  return res.json(movies);
 });
 
-app.listen(8080, () => {
-  console.log("Now listening on PORT 8080");
+app.get("/movies/:id", (req, res) => {
+  const id = req.params?.id;
+  const movie = movies.find((movie) => movie.id === Number(id));
+  if (!movie) {
+    return res.status(404).json({ error: "Movie not found" });
+  }
+  res.json(movie);
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Not found" });
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
