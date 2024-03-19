69f5f02bb41de878a6cc938913ba96886df18c08
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-19 06:44:51 +0300

    feat: Refactor application structure for dynamic routing
    - Reorganized application structure to utilize React Router DOM's Outlet for dynamic page rendering
    - Improved modularity and flexibility in application navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
-import Home from "./pages/home/Home";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <>
+      <Outlet />
+    </>
   );
 }
 
