0ec3e6ce9b0456e3aba62aebecab3a5d18096930
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-20 13:45:55 +0300

    feat: Seed movies data into database from JSON file
    - Created a new file to seed movie data into database
    - Utilized Prisma to interact with the database and insert movie records

---
+++ server/scripts/seedMovies.js
@@ -0,0 +1,21 @@
+const movies = require("../movies.json");
+const { prisma } = require("../db");
+
+const seedMovies = async () => {
+  const moviesFormatted = movies.map(
+    ({ title, description, thumbnailUrl, videoUrl, duration, genre }) => {
+      return {
+        title,
+        description,
+        thumbnailUrl,
+        videoUrl,
+        duration,
+        genre,
+      };
+    }
+  );
+
+  await prisma.movie.createMany({ data: moviesFormatted });
+};
+
+seedMovies();
