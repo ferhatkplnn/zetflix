1f2bc710876e98dd188ce209b4ff5bbd1bcb3ca7
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-20 21:49:18 +0300

    feat: Add user signup route with validation and password hashing
    - Created new file 'auth.js' to handle user authentication routes
    - Implemented signup route with input validation for email, password, and username
    - Hashed user password securely using bcrypt before storing in the database

---
+++ server/routes/auth.js
@@ -0,0 +1,57 @@
+const router = require("express").Router();
+const { check, validationResult } = require("express-validator");
+const { prisma } = require("../db");
+const bcrypt = require("bcrypt");
+
+router.post(
+  "/signup",
+  [
+    check("email", "Please input a valid email").isEmail(),
+    check(
+      "password",
+      "Please input a password with a min length of 6"
+    ).isLength({ min: 6 }),
+    check(
+      "username",
+      "Please input a username with a min lenght of 6"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+    const { email, password, username } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (user) {
+      return res
+        .status(400)
+        .json({ errors: [{ msg: "This user already exists." }] });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        username,
+        password: hashedPassword,
+      },
+    });
+
+    res.json({
+      user: newUser,
+    });
+  }
+);
+
+module.exports = router;
