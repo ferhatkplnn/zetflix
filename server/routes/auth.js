7b7f59bcc9fc1326def2df008ae6e8eb17bc4380
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-21 09:16:48 +0300

    feat: Implement user authentication with JWT
    - Update /signup endpoint to return JWT token upon user registration
    - Add /login endpoint for user login with JWT token generation

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const { check, validationResult } = require("express-validator");
 const { prisma } = require("../db");
 const bcrypt = require("bcrypt");
+const JWT = require("jsonwebtoken");
 
 router.post(
   "/signup",
@@ -46,12 +47,61 @@ router.post(
         username,
         password: hashedPassword,
       },
+      select: {
+        id: true,
+        username: true,
+        email: true,
+      },
+    });
+
+    const token = JWT.sign(newUser, process.env.JWT_SECRET, {
+      expiresIn: 3600000,
     });
 
     res.json({
       user: newUser,
+      token,
     });
   }
 );
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!user) {
+    return res.status(400).json({
+      erros: [{ msg: "Invalid  credentials" }],
+    });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return res.status(400).json({
+      erros: [{ msg: "Invalid  credentials" }],
+    });
+  }
+
+  const userPayload = {
+    id: user.id,
+    email: user.email,
+    username: user.username,
+  };
+
+  const token = JWT.sign(userPayload, process.env.JWT_SECRET, {
+    expiresIn: 3600000,
+  });
+
+  res.json({
+    user: userPayload,
+    token,
+  });
+});
 
 module.exports = router;
