a090e92d73e6b8ebff2bc3269111eb9302f38ca3
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-21 12:36:36 +0300

    feat: React Hook Form integration and null pointer prevention in authentication form
    - Integrated react-hook-form for easier form management
    - Implemented context API for form elements registration
    - Added variant state for managing user login or signup status
    - Updated form elements and submission function using react-hook-form
    - Introduced null check to prevent potential null pointer exceptions

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,13 +1,40 @@
-import { useState } from "react";
+import { createContext, useState } from "react";
 import NavBar from "../../components/navbar/Navbar";
 import Input from "../../components/shared/Input";
+import { useForm, SubmitHandler, UseFormRegister } from "react-hook-form";
+
+export type Inputs = {
+  email: string;
+  username: string;
+  password: string;
+};
 
 enum Variant {
   SIGN_UP,
   LOGIN_IN,
 }
 
+interface AuthFormContextType {
+  register: UseFormRegister<Inputs> | null;
+}
+
+export const AuthFormContext = createContext<AuthFormContextType>({
+  register: null,
+});
+
 function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+    console.log(watch(["email", "password"]));
+  };
+
   const [variant, setVariant] = useState(Variant.LOGIN_IN);
 
   return (
@@ -18,18 +45,23 @@ function Login() {
           <h2 className="text-white text-4xl mb-8 font-semibold">
             {variant === Variant.LOGIN_IN ? "Sign in" : "Sign up"}
           </h2>
-          <form className="flex flex-col gap-4">
-            <Input label="Email address" name="email" type="email" />
-            {variant === Variant.SIGN_UP && (
-              <Input label="Username" name="username" type="text" />
-            )}
-            <Input label="Password" name="password" type="password" />
-            <input
-              type="submit"
-              value="Submit"
-              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 cursor-pointer duration-300"
-            />
-          </form>
+          <AuthFormContext.Provider value={{ register }}>
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="flex flex-col gap-4"
+            >
+              <Input label="Email address" name="email" type="email" />
+              {variant === Variant.SIGN_UP && (
+                <Input label="Username" name="username" type="text" />
+              )}
+              <Input label="Password" name="password" type="password" />
+              <input
+                type="submit"
+                value="Submit"
+                className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 cursor-pointer duration-300"
+              />
+            </form>
+          </AuthFormContext.Provider>
           {variant === Variant.LOGIN_IN ? (
             <p
               className="text-neutral-500 mt-12"
