66609a4b7d4425e5548e88f2b3a874055c26e274
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-21 11:13:52 +0300

    feat: Input component enhancements
    - Added 'name' parameter to Input component for form submission
    - Updated assignment of 'id' attribute to ensure consistency with label

--- client/src/components/shared/Input.tsx
+++ client/src/components/shared/Input.tsx
@@ -2,19 +2,21 @@ import { ComponentPropsWithoutRef } from "react";
 
 type InputProps = ComponentPropsWithoutRef<"input"> & {
   label: string;
+  name: string;
 };
 
-function Input({ label, type }: InputProps) {
+function Input({ label, name, type }: InputProps) {
   return (
     <div className="relative">
       <input
-        id={label}
+        id={name}
+        name={name}
         type={type}
         placeholder=""
         className="peer bg-black bg-opacity-50 px-4 pt-6 pb-2 w-full rounded border border-slate-600 text-white focus:ring-2 focus:ring-white outline-none invalid:border-red-500"
       />
       <label
-        htmlFor={label}
+        htmlFor={name}
         className="absolute text-slate-400 left-3 scale-75 top-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:top-4 peer-focus:scale-75 peer-focus:top-2 duration-300"
       >
         {label}
