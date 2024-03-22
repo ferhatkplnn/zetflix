f16f937935411e673f478bf351df6547bc3c9ff9
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-23 00:59:36 +0300

    feat: Integrate user authentication into main App component
    - Added fetchUser function to retrieve user information on component mount
    - Integrated useAuth hook to handle user authentication in App component
    - Improved user experience by automatically fetching user data on app startup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,13 @@
 import { Outlet } from "react-router-dom";
+import useAuth from "./hooks/useAuth";
+import { useEffect } from "react";
 
 function App() {
+  const { fetchUser } = useAuth();
+
+  useEffect(() => {
+    fetchUser();
+  }, [fetchUser]);
   return (
     <>
       <Outlet />
