fc4b47c30b930f6e5a5c0ec8af3346c69ca69b23
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-23 01:00:40 +0300

    feat: Add error handling to movie list endpoint
    - Implemented try-catch block to handle exceptions in movie list endpoint
    - Ensured robust error management for better server stability

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -2,13 +2,17 @@ const router = require("express").Router();
 const { prisma } = require("../db");
 
 router.get("/movies/list", async (req, res) => {
-  const offset = Number(req.query?.offset);
-  const count = await prisma.movie.count();
-  const movies = await prisma.movie.findMany({
-    take: 12,
-    skip: offset,
-  });
-  return res.json({ movies, limit: count });
+  try {
+    const offset = Number(req.query?.offset);
+    const count = await prisma.movie.count();
+    const movies = await prisma.movie.findMany({
+      take: 12,
+      skip: offset,
+    });
+    return res.json({ movies, limit: count });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get("/movies/:id", async (req, res) => {
