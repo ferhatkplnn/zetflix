d84aac6e9ec5cbc100a06b170c6a6800683d90b5
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-23 15:36:45 +0300

    refactor: Remove redundant Redux user state retrieval in Browse component
    - Removed unnecessary Redux user state retrieval in the Browse component
    - Simplified the component by removing unnecessary code related to user state

--- client/src/pages/browse/Browse.tsx
+++ client/src/pages/browse/Browse.tsx
@@ -3,20 +3,12 @@ import NavBar from "../../components/navbar/Navbar";
 import useMoviesList from "../../hooks/useMoviesList";
 import BillBoard from "./components/BillBoard";
 import MovieList from "./components/MovieList";
-import { useSelector } from "react-redux";
-import { RootState } from "../../app/store";
 
 function Browse() {
   const [offset, setOffset] = useState<number>(0);
   const { data, loading, error } = useMoviesList(offset);
 
   const observer = useRef<null | IntersectionObserver>(null);
-
-  const { isLoading, user } = useSelector(
-    (state: RootState) => state.user.value
-  );
-
-  console.log({ isLoading, user });
 
   const lastElementRef = useCallback((node: HTMLDivElement) => {
     if (observer.current) observer.current.disconnect();
