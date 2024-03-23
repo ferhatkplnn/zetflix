a0f5d58d77d407ae066105a632440ed748e4aee5
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-23 17:21:41 +0300

    feat: Update PlanCard component to accept number type for price and add dynamic rendering for PlanCardItem
    - Modified PlanCardProps to accept number type for price
    - Removed default parameter values for downloads and southPark in PlanCardProps since they are explicitly provided
    - Updated PlanCardItem components to render dynamically based on the type of feature (price or other features)

--- client/src/pages/plans/components/PlanCard.tsx
+++ client/src/pages/plans/components/PlanCard.tsx
@@ -2,38 +2,37 @@ import PlanCardItem from "./PlanCardItem";
 
 type PlanCardProps = {
   type: string;
-  price: string;
+  price: string | number;
   downloads: boolean;
   southPark: boolean;
 };
 
-function PlanCard({
-  type,
-  price,
-  downloads = true,
-  southPark = true,
-}: PlanCardProps) {
+function PlanCard({ type, price, downloads, southPark }: PlanCardProps) {
+  console.log({ downloads, southPark });
   return (
     <div className="border rounded p-3 h-[350px] w-full pointer mr-3">
       <div className="rounded bg-gradient-to-r from-cyan-500 to-blue-500 w-full p-3 text-white font-bold">
         <h3 className="text-2xl">{type}</h3>
-        <p className="font-light">{price}</p>
+        <p className="font-light">${price}</p>
       </div>
 
       <PlanCardItem
         condition={true}
         title="Monthly Price"
         description={price}
+        type="price"
       />
       <PlanCardItem
         condition={downloads}
         title="Downloads"
         description="Included"
+        type="feature"
       />
       <PlanCardItem
         condition={southPark}
         title="South Park"
         description="Included"
+        type="feature"
       />
     </div>
   );
