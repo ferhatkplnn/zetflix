a5aaeef4ac7ee093425d5eaac0fd7e4bc5082c1e
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-23 16:18:37 +0300

    feat: Add subscription endpoint
    - Added a new endpoint for subscription management
    - Introduced '/sub' endpoint to handle subscription-related functionalities

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ app.use(cors());
 app.use(express.json());
 app.use("", require("./routes/movies"));
 app.use("/auth", require("./routes/auth"));
+app.use("/sub", require("./routes/sub"));
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
