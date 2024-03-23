be8fad2651de810407f247b7f74cbf28dd1940e8
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-23 16:44:11 +0300

    feat: Enhance product listing endpoint with price details
    - Updated product listing endpoint to include price details for each product
    - Simplified the structure of product data for better readability

--- server/routes/sub.js
+++ server/routes/sub.js
@@ -1,9 +1,20 @@
 const router = require("express").Router();
 const { stripe } = require("../utils/stripe");
 router.get("/products", async (req, res) => {
-  const response = await stripe.products.list({ apiKey: process.env.STRIPE_SECRET });
+  const response = await stripe.products.list({ expand: ["data.default_price"] });
 
-  return res.json(response.data);
+  const products = response.data.map(({ id, name, default_price }) => {
+    return {
+      id,
+      name,
+      price: {
+        amount: default_price.unit_amount,
+        id: default_price.id,
+      },
+    };
+  });
+
+  return res.json(products);
 });
 
 module.exports = router;
