5a1063a652ff7b2892b5ec5e653bc1a038003d91
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-24 15:05:58 +0300

    feat: Enable plan selection in PlanCard component
    - Added onClick functionality to PlanCard for selecting plans
    - Introduced selectedSession and setSelectedSession props for managing selected plan ID
    - Updated styling to visually highlight selected plan by adding a border

--- client/src/pages/plans/components/PlanCard.tsx
+++ client/src/pages/plans/components/PlanCard.tsx
@@ -5,12 +5,27 @@ type PlanCardProps = {
   price: string | number;
   downloads: boolean;
   southPark: boolean;
+  selectedSession: string | null;
+  setSelectedSession: React.Dispatch<React.SetStateAction<string | null>>;
+  id: string;
 };
 
-function PlanCard({ type, price, downloads, southPark }: PlanCardProps) {
-  console.log({ downloads, southPark });
+function PlanCard({
+  type,
+  price,
+  downloads,
+  southPark,
+  selectedSession,
+  setSelectedSession,
+  id,
+}: PlanCardProps) {
   return (
-    <div className="border rounded p-3 h-[350px] w-full pointer mr-3">
+    <div
+      onClick={() => setSelectedSession(id)}
+      className={`border rounded p-3 h-[350px] w-full pointer mr-3 cursor-pointer ${
+        selectedSession === id ? "border-3 border-black" : ""
+      }`}
+    >
       <div className="rounded bg-gradient-to-r from-cyan-500 to-blue-500 w-full p-3 text-white font-bold">
         <h3 className="text-2xl">{type}</h3>
         <p className="font-light">${price}</p>
