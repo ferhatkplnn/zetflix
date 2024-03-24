9cceb0906418cabf3deca8667290754819ba2a60
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-24 15:05:17 +0300

    feat: User session handling for purchase functionality
    - Introduce useState for managing selected session ID
    - Implement createSession function to initiate purchase session
    - Utilize Redux useSelector for retrieving user information
    - Disable purchase button until a plan is selected
    - Update PlanCard component to include unique plan IDs

--- client/src/pages/plans/Plans.tsx
+++ client/src/pages/plans/Plans.tsx
@@ -1,13 +1,34 @@
+import { useState } from "react";
 import usePlans from "../../hooks/usePlans";
 import PlanCard from "./components/PlanCard";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { RootState } from "../../app/store";
+
+const createSession = async (email: string, priceId: string) => {
+  const response = await axios.post("http://localhost:8080/sub/session", {
+    email,
+    priceId,
+  });
+
+  const { url } = response.data;
+
+  window.location.href = url;
+};
 
 function Plans() {
   const { loading, data, error } = usePlans();
-
-  console.log("Plans is rendered");
+  const [selectedSession, setSelectedSession] = useState<null | string>(null);
+  const { user } = useSelector((state: RootState) => state.user.value);
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>{error}</div>;
+
+  const handleClick = () => {
+    if (user && selectedSession) {
+      createSession(user?.email, selectedSession);
+    }
+  };
 
   return (
     <div className="flex items-center h-screen justify-center">
@@ -22,12 +43,19 @@ function Plans() {
                 key={index}
                 type={plan.name}
                 price={plan.price.amount}
+                id={plan.price.id}
                 downloads={plan.canDownload}
                 southPark={plan.canWatchSouth}
+                selectedSession={selectedSession}
+                setSelectedSession={setSelectedSession}
               />
             ))}
         </div>
-        <button className="bg-red-500 py-3 text-white rounded-md w-full mt-4 hover:bg-red-700 cursor-pointer duration-300">
+        <button
+          className="bg-red-500 py-3 text-white rounded-md w-full mt-4 hover:bg-red-700 cursor-pointer duration-300 disabled:bg-gray-400 disabled:cursor-default"
+          disabled={!selectedSession}
+          onClick={handleClick}
+        >
           Purchase
         </button>
       </div>
